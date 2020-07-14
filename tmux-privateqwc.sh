dc8a593a26cb55874a638059e6941978e4e744e4
gladcow <jahr@yandex.ru>
2020-07-14 17:30:07 +0300

    use fixed difficuty

--- tmux-privateqwc.sh
+++ tmux-privateqwc.sh
@@ -11,20 +11,19 @@ tmux kill-session -t $SN
 cd $TESTNET_PATH 
 
 
-# nodes window
-
+# nodes windo
 # start node_01 (initial session)
-tmux new-session -d -s $SN -n nodes -- sh -ic './qwertycoind --p2p-bind-port 58080 --rpc-bind-port 58081 --hide-my-port --data-dir node_01 --p2p-bind-ip 127.0.0.1 --log-level 2 --add-exclusive-node 127.0.0.1:38080 --add-exclusive-node 127.0.0.1:48080 || read WHATEVER'
+tmux new-session -d -s $SN -n nodes -- sh -ic './qwertycoind --p2p-bind-port 58080 --rpc-bind-port 58081 --hide-my-port --data-dir node_01 --p2p-bind-ip 127.0.0.1 --log-level 2 --fixed-difficulty 1 --add-exclusive-node 127.0.0.1:38080 --add-exclusive-node 127.0.0.1:48080 || read WHATEVER'
 
 # start node_02
 tmux split-window -dv
 tmux select-pane -t 1
 tmux split-window -dh      
-tmux send './qwertycoind --p2p-bind-port 38080 --rpc-bind-port 38081 --hide-my-port --log-level 2 --data-dir node_02 --p2p-bind-ip 127.0.0.1 --add-exclusive-node 127.0.0.1:58080 --add-exclusive-node 127.0.0.1:48080 || read WHATEVER' ENTER
+tmux send './qwertycoind --p2p-bind-port 38080 --rpc-bind-port 38081 --hide-my-port --log-level 2 --fixed-difficulty 1 --data-dir node_02 --p2p-bind-ip 127.0.0.1 --add-exclusive-node 127.0.0.1:58080 --add-exclusive-node 127.0.0.1:48080 || read WHATEVER' ENTER
 
 # start node_03
 tmux select-pane -t 2
-tmux send './qwertycoind --p2p-bind-port 48080 --rpc-bind-port 48081 --hide-my-port --log-level 2 --data-dir node_03 --p2p-bind-ip 127.0.0.1 --add-exclusive-node 127.0.0.1:58080 --add-exclusive-node 127.0.0.1:38080 || read WHATEVER' ENTER
+tmux send './qwertycoind --p2p-bind-port 48080 --rpc-bind-port 48081 --hide-my-port --log-level 2 --fixed-difficulty 1 --data-dir node_03 --p2p-bind-ip 127.0.0.1 --add-exclusive-node 127.0.0.1:58080 --add-exclusive-node 127.0.0.1:38080 || read WHATEVER' ENTER
 
 # wallets window
 
